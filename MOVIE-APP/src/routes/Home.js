fc5095d68f055c9c5218daab41a558de51a30f6d
주상민 <besforyou2@gmail.com>
2023-10-06 16:33:50 +0900

    feat: Search 컴포넌트 & 버튼

--- MOVIE-APP/src/routes/Home.js
+++ MOVIE-APP/src/routes/Home.js
@@ -1,13 +1,17 @@
 import { Component } from "../core/heropy";
 import Headline from '../components/Headline'
+import Search from '../components/Search'
 
 export default class Home extends Component {
   render() {
     const headline = new Headline().el
-    // Home 컴포넌트는 container 이름을 가진 div로 만들어지고
+    const search = new Search().el
+
+
     this.el.classList.add('container')
     this.el.append(
-      headline // headline 컴포넌트가 append 될것
+      headline,
+      search
     )
   }
 }
