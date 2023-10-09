d6e5248a3ee209878c7548b93d55612f233612b3
주상민 <besforyou2@gmail.com>
2023-10-09 23:16:01 +0900

    feat: 영화 상세 정보 불러오기

--- MOVIE-APP/src/routes/index.js
+++ MOVIE-APP/src/routes/index.js
@@ -1,7 +1,9 @@
 import { createRouter } from '../core/heropy'
 import Home from './Home'
+import Movie from './Movie'
 
 // createRouter : 페이지를 구분하는 다양한 옵션을 받을 수 있음.
 export default createRouter([
-  { path: '#/', component: Home }
+  { path: '#/', component: Home },
+  { path: '#/movie', component: Movie}
 ])
