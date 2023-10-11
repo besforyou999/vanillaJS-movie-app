c5f4cb9bba7114cca5ab19c38b05dfaeea9bda50
주상민 <besforyou2@gmail.com>
2023-10-11 22:55:25 +0900

    feat: 내 정보 페이지 (About) 출력

--- MOVIE-APP/src/routes/index.js
+++ MOVIE-APP/src/routes/index.js
@@ -1,10 +1,12 @@
 import { createRouter } from '../core/heropy'
 import Home from './Home'
 import Movie from './Movie'
+import About from './About'
 
 // createRouter : 페이지를 구분하는 다양한 옵션을 받을 수 있음.
 export default createRouter([
   { path: '#/', component: Home },
-  { path: '#/movie', component: Movie}
+  { path: '#/movie', component: Movie },
+  { path: '#/about', component: About }
 ])
 
