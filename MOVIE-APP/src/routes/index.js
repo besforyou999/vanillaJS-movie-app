0c65874b86c397ea7124c7e74ee13e5068fbfe30
주상민 <besforyou2@gmail.com>
2023-10-11 23:19:07 +0900

    feat: 반응형 레이아웃 with media query

--- MOVIE-APP/src/routes/index.js
+++ MOVIE-APP/src/routes/index.js
@@ -2,11 +2,13 @@ import { createRouter } from '../core/heropy'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 
 // createRouter : 페이지를 구분하는 다양한 옵션을 받을 수 있음.
 export default createRouter([
   { path: '#/', component: Home },
   { path: '#/movie', component: Movie },
-  { path: '#/about', component: About }
+  { path: '#/about', component: About },
+  { path: '.*', component: NotFound }
 ])
 
