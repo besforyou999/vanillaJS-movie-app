277903064607890bf232ccc6f7c2b267192b7941
주상민 <besforyou2@gmail.com>
2023-10-12 17:18:37 +0900

    feat: 코드 정리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TheFooter from './components/TheFooter'
 
 export default class App extends Component {
   render() {
+    
     const theHeader = new TheHeader().el;
     const theFooter = new TheFooter().el;
     const routerView = document.createElement('router-view')
